f6bfd8905677201424da58de677d271da02f4d2a
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-12-08 11:23:23 +0530

    update

--- CreativeProjects/progress-monitor/main.js
+++ CreativeProjects/progress-monitor/main.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.getElementById('study-hours-form');
-    
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent the default form submission
-      
-      const studyHours = document.getElementById('study-hours').value;
-      console.log('Number of hours studied:', studyHours);
-      
-      // You can now use the studyHours variable as needed
-    });
-  });
+
+
+function m() {
+let monday = document.getElementById("name").value;
+console.log(monday);
+return false;
+}
+print(monday);
+// t();
+// w();
+// th();
+// f();
+// s();
+// su();
+
